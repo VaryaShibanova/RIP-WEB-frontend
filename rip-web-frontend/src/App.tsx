eda306b6bdc2c5f10605df5e6aa21e76ba4e42dd
Varya Shibanova <v4013345@gmail.com>
2025-11-03 22:46:34 +0300

    lab6

--- rip-web-frontend/src/App.tsx
+++ rip-web-frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { useEffect } from 'react';
+import type { FC } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import CustomNavbar from './components/Navbar';
@@ -8,26 +9,36 @@ import AnomalyDetailPage from './pages/AnomalyDetailPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-const App: React.FC = () => {
+const App: FC = () => {
+  useEffect(() => {
+    if ((window as any).TAURI) {
+      console.log('Tauri is available');
+    } else {
+      console.log('Running in browser mode');
+    }
+  }, []);
+
   return (
     <Router>
-      <div className="App">
+      <div className="d-flex flex-column min-vh-100">
         <CustomNavbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/anomalies" element={<AnomaliesPage />} />
-          <Route path="/anomalies/:id" element={<AnomalyDetailPage />} />
-          <Route path="/my-request" element={
-            <div className="home-page">
-              <Container className="page-container"> 
-                <div className="text-center">
-                  <h1 style={{ color: 'white', marginBottom: '30px' }}>Моя заявка</h1>
-                  <p style={{ color: 'white' }}>Страница в разработке...</p>
-                </div>
-              </Container>
-            </div>
-          } />
-        </Routes>
+        <main className="flex-grow-1 py-4">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/anomalies" element={<AnomaliesPage />} />
+            <Route path="/anomalies/:id" element={<AnomalyDetailPage />} />
+            <Route path="/my-request" element={
+              <div className="home-page">
+                <Container className="page-container"> 
+                  <div className="text-center">
+                    <h1 style={{ color: 'white', marginBottom: '30px' }}>Моя заявка</h1>
+                    <p style={{ color: 'white' }}>Страница в разработке...</p>
+                  </div>
+                </Container>
+              </div>
+            } />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
