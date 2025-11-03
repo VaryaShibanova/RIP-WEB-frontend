4be527388baccea804a431acd279715739478bc7
Varya Shibanova <v4013345@gmail.com>
2025-11-03 18:58:47 +0300

    lab5

--- rip-web-frontend/vite.config.ts
+++ rip-web-frontend/vite.config.ts
@@ -2,21 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: 'classic' // Добавьте эту опцию
+  })],
   server: {
     proxy: {
       "/api": {
         target: "http://localhost:8080",
         changeOrigin: true,
-        // УБРАТЬ rewrite - бэкенд должен получать /api/anomalies
-        // rewrite: (path) => path.replace(/^\/api/, "/"),
       },
     },
     watch: {
-        usePolling: true,
+      usePolling: true,
     }, 
     host: true,
     strictPort: true,
     port: 3000,
   },
-});
+})
