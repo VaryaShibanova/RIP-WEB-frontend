44692bab281183856e8ca62753de000022c6118d
Varya Shibanova <v4013345@gmail.com>
2025-11-04 18:44:21 +0300

    lab6

--- rip-web-frontend/dev-dist/sw.js
+++ rip-web-frontend/dev-dist/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-ced28f87'], (function (workbox) { 'use strict';
+define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
 
   self.skipWaiting();
   workbox.clientsClaim();
@@ -79,7 +79,7 @@ define(['./workbox-ced28f87'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.6cr6lfjualg"
+    "revision": "0.u2b36khid2g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
