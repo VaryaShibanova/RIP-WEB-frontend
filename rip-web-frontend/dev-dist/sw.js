b9c3de32129ed9deff29c2941c5365f85ed613cd
Varya Shibanova <v4013345@gmail.com>
2025-12-07 16:01:58 +0300

    lab8

--- rip-web-frontend/dev-dist/sw.js
+++ rip-web-frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.0e18nd8ktlg"
+    "revision": "0.fjla4b7crfg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
